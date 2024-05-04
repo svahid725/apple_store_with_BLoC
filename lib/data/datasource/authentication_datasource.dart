10c7bf313e1e4e088f6e015ba54762c778a30826
vahid.sadr <vahidsadrolashrafi@gmail.com>
2024-05-04 17:17:18 +0330

    [Feat]: implement IAuthenticationDatasource interface for AuthenticationRemote class

--- lib/data/datasource/authentication_datasource.dart
+++ lib/data/datasource/authentication_datasource.dart
@@ -2,19 +2,19 @@ import 'package:apple_store/di/di.dart';
 import 'package:apple_store/util/api_exception.dart';
 import 'package:dio/dio.dart';
 
-class AuthenticationRemote {
+abstract class IAuthenticationDatasource {
+  Future<void> register(String username, String password, String passwordConfirm);
+}
+
+class AuthenticationRemote implements IAuthenticationDatasource {
   final Dio _dio = locator.get();
 
+  @override
   Future<void> register(
     String username,
     String password,
     String passwordConfirm,
   ) async {
-    Map<String, String> data = {
-      'username': username,
-      'password': password,
-      'passwordConfirm': passwordConfirm,
-    };
     Response response;
     try {
       response = await _dio.post(
@@ -25,15 +25,13 @@ class AuthenticationRemote {
           'passwordConfirm': passwordConfirm,
         },
       );
-      if (response.statusCode == 200){
+      if (response.statusCode == 200) {
         print(response.data);
       }
     } on DioError catch (ex) {
-      throw ApiException(
-        code: ex.response?.statusCode ,
-        message: ex.response?.data['message']
-      );
-
-    } catch (ex) {}
+      throw ApiException(code: ex.response?.statusCode, message: ex.response?.data['message']);
+    } catch (ex) {
+      throw ApiException(code: 0, message: 'unknown error');
+    }
   }
 }
