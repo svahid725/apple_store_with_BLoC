6bfe24f073962330a61fdb09add7fcc901a693e6
vahid.sadr <vahidsadrolashrafi@gmail.com>
2024-05-04 17:16:09 +0330

    [Feat]: add register method for register user

--- lib/data/repository/authentication_repository.dart
+++ lib/data/repository/authentication_repository.dart
@@ -1,5 +1,17 @@
 import 'package:apple_store/data/datasource/authentication_datasource.dart';
+import 'package:apple_store/di/di.dart';
+import 'package:apple_store/util/api_exception.dart';
+import 'package:dartz/dartz.dart';
 
 class AuthenticationRepository {
-  final AuthenticationRemote dataResourse =
+  final IAuthenticationDatasource _dataSource = locator.get();
+
+  Future<Either<String, String>> register() async {
+    try {
+      await _dataSource.register('test123', '12345678', '12345678');
+      return right('ثبت نام انجام شد');
+    } on ApiException catch (ex) {
+      return left(ex.message ?? 'خطا محتوای متنی ندارد');
+    }
+  }
 }
