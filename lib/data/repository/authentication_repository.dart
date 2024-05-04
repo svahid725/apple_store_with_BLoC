af7b355f7a64a3f31849ef14c1e71a2e40f9c54e
vahid.sadr <vahidsadrolashrafi@gmail.com>
2024-05-04 20:17:10 +0330

    [Feat]: add login abstract method for IAuthRepository class and use it in AuthenticationRepository

--- lib/data/repository/authentication_repository.dart
+++ lib/data/repository/authentication_repository.dart
@@ -5,6 +5,8 @@ import 'package:dartz/dartz.dart';
 
 abstract class IAuthRepository {
   Future<Either<String, String>> register(String username, String password, String passwordConfirm);
+
+  Future<Either<String, String>> login(String username, String password);
 }
 
 class AuthenticationRepository implements IAuthRepository {
@@ -19,6 +21,20 @@ class AuthenticationRepository implements IAuthRepository {
     try {
       await _dataSource.register('test123', '12345678', '12345678');
       return right('ثبت نام انجام شد');
+    } on ApiException catch (ex) {
+      return left(ex.message ?? 'خطا محتوای متنی ندارد');
+    }
+  }
+
+  @override
+  Future<Either<String, String>> login(String username, String password) async {
+    try {
+      String token = await _dataSource.login(username, password);
+      if (token.isNotEmpty) {
+        return Right('شما وارد شديد');
+      } else {
+        return left('ورود ناموفق');
+      }
     } on ApiException catch (ex) {
       return left(ex.message ?? 'خطا محتوای متنی ندارد');
     }
