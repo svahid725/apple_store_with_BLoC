b34c72fa4a6eca971076592238869ea1368102e5
vahid.sadr <vahidsadrolashrafi@gmail.com>
2024-05-04 17:28:31 +0330

    [Feat]: implement IAuthRepository interface for AuthenticationRepository class

--- lib/data/repository/authentication_repository.dart
+++ lib/data/repository/authentication_repository.dart
@@ -3,10 +3,19 @@ import 'package:apple_store/di/di.dart';
 import 'package:apple_store/util/api_exception.dart';
 import 'package:dartz/dartz.dart';
 
-class AuthenticationRepository {
+abstract class IAuthRepository {
+  Future<Either<String, String>> register(String username, String password, String passwordConfirm);
+}
+
+class AuthenticationRepository implements IAuthRepository {
   final IAuthenticationDatasource _dataSource = locator.get();
 
-  Future<Either<String, String>> register() async {
+  @override
+  Future<Either<String, String>> register(
+    String username,
+    String password,
+    String passwordConfirm,
+  ) async {
     try {
       await _dataSource.register('test123', '12345678', '12345678');
       return right('ثبت نام انجام شد');
