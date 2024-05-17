cfd6aebe2314e3fc2e34fc4ef22d259a90e91185
vahid.sadr <vahidsadrolashrafi@gmail.com>
2024-05-17 14:02:15 +0330

    [Feat]: implement class constructor and formJson named constructor

--- lib/data/model/category.dart
+++ lib/data/model/category.dart
@@ -1,3 +1,27 @@
-class Category{
+class Category {
+  String? collectionId;
+  String? id;
+  String? icon;
+  String? thumbnail;
+  String? title;
+  String? color;
 
+  Category(
+      this.collectionId,
+      this.id,
+      this.icon,
+      this.thumbnail,
+      this.title,
+      this.color,
+      );
+
+  factory Category.fromJson(Map<String, dynamic> json) =>
+      Category(
+        json['collectionId'],
+        json['id'],
+        json['icon'],
+        json['thumbnail'],
+        json['title'],
+        json['color']
+      );
 }
