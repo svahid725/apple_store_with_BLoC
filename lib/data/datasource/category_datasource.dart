d1e020a4a7aa6402f60b3c7854ece149efa81852
vahid.sadr <vahidsadrolashrafi@gmail.com>
2024-05-17 14:01:23 +0330

    [Feat]: implement ICategoryDatasource interface and CategoryRemoteDatasource and getCategories method

---
+++ lib/data/datasource/category_datasource.dart
@@ -0,0 +1,30 @@
+import 'package:apple_store/data/model/category.dart';
+import 'package:apple_store/di/di.dart';
+import 'package:apple_store/util/api_exception.dart';
+import 'package:dio/dio.dart';
+
+abstract class ICategoryDatasource {
+  Future<List<Category>> getCategories();
+}
+
+class CategoryRemoteDatasource extends ICategoryDatasource {
+  final Dio _dio = locator.get();
+
+  @override
+  Future<List<Category>> getCategories() async {
+    try {
+      Response response = await _dio.post('collections/category/records');
+      return response.data['items'].map<Category>(Category.fromJson).toList();
+    } on DioError catch (ex) {
+      throw ApiException(
+        code: ex.response?.statusCode,
+        message: ex.response?.data['message'],
+      );
+    } catch (ex) {
+      throw ApiException(
+        code: 0,
+        message: 'unknown error',
+      );
+    }
+  }
+}
