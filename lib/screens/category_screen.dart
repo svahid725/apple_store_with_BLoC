ec0d131047f0a11abd91a770d125d081e36b4441
vahid.sadr <vahidsadrolashrafi@gmail.com>
2024-05-18 23:33:14 +0330

    [Feat]: category connected to api

--- lib/screens/category_screen.dart
+++ lib/screens/category_screen.dart
@@ -1,13 +1,34 @@
+import 'package:apple_store/bloc/category/category_bloc.dart';
+import 'package:apple_store/bloc/category/category_event.dart';
+import 'package:apple_store/bloc/category/category_state.dart';
+import 'package:apple_store/constants/app_theme.dart';
 import 'package:apple_store/constants/font_styles.dart';
+import 'package:apple_store/data/model/category.dart';
+import 'package:apple_store/data/repository/category_repository.dart';
+import 'package:apple_store/widgets/cached_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class CategoryScreen extends StatelessWidget {
+class CategoryScreen extends StatefulWidget {
   const CategoryScreen({super.key});
+
+  @override
+  State<CategoryScreen> createState() => _CategoryScreenState();
+}
+
+class _CategoryScreenState extends State<CategoryScreen> {
+  @override
+  void initState() {
+    BlocProvider.of<CategoryBloc>(context).add(CategoryRequestList());
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: CustomScrollView(
+      backgroundColor: AppColors.backgroundScreenColor,
+      body: SafeArea(
+          child: CustomScrollView(
         slivers: [
           SliverToBoxAdapter(
             child: Padding(
@@ -15,40 +36,75 @@ class CategoryScreen extends StatelessWidget {
               child: Container(
                 height: 46,
                 decoration: const BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.all(Radius.circular(15))
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(Radius.circular(15)),
                 ),
                 child: Row(
                   children: [
-                    const SizedBox(width: 16,),
+                    const SizedBox(
+                      width: 16,
+                    ),
                     Image.asset('assets/images/icon_apple_blue.png'),
-                    const Expanded(child: Text(
-                      'دسته بندی',
-                      style: t16sb,
-                      textAlign: TextAlign.center,
-                    ),),
-                    const SizedBox(width: 10,),
+                    const Expanded(
+                      child: Text(
+                        'دسته بندی',
+                        style: t16sb,
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
                   ],
                 ),
               ),
             ),
           ),
-          SliverPadding(
-            padding: const EdgeInsets.symmetric(horizontal: 44),
-            sliver: SliverGrid(delegate: SliverChildBuilderDelegate((context, index) => Container(
-              decoration: const BoxDecoration(
-                color: Colors.red,
-                borderRadius: BorderRadius.all(Radius.circular(15))
-              ),
-            )), gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              mainAxisSpacing: 20,
-              crossAxisSpacing: 20,
-            )),
+          BlocBuilder<CategoryBloc, CategoryState>(
+            builder: (context, state) {
+              if (state is CategoryLoadingState) {
+                return const SliverToBoxAdapter(child: CircularProgressIndicator());
+              }
+              if (state is CategoryResponseState) {
+                return state.response.fold((l) {
+                  return SliverToBoxAdapter(
+                    child: Text(l),
+                  );
+                }, (r) {
+                  return _listCategory(categoryList: r);
+                });
+              }
+              return const SliverToBoxAdapter(child: Center(child: Text('error')));
+            },
           )
-
         ],
       )),
+    );
+  }
+}
+
+class _listCategory extends StatelessWidget {
+  List<Category> categoryList;
+
+  _listCategory({super.key, required this.categoryList});
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverPadding(
+      padding: const EdgeInsets.symmetric(horizontal: 44),
+      sliver: SliverGrid(
+        delegate: SliverChildBuilderDelegate(
+          (context, index) => CachedImage(
+            imageUrl: categoryList[index].thumbnail!,
+          ),
+          childCount: categoryList.length,
+        ),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          mainAxisSpacing: 20,
+          crossAxisSpacing: 20,
+        ),
+      ),
     );
   }
 }
